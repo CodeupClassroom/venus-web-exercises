7ffc5de90911bcf33afbc0ac9460b6ab7df187b3
Docrob <mark.robinson@codeup.com>
2022-06-28 09:04:16 -0500

    morning practice

--- practice/jun28.js
+++ practice/jun28.js
@@ -1,37 +1,43 @@
-
-let hello = "hello world";
-let myName = "bob";
-let myLastName = "smith";
-
-// console.log(hello);
-
 /*
-AND truth table
-OR truth table
-*** find a reference
+COMMAND LOOPS
+
+a loop that:
+ 1. accepts a command from the user
+ 2. acts on the command if the command is valid
+ 3. continues looping, waiting for the next command
+ 4. EXITS when the user issues an exit command
 
  */
-if(myName === "bob" && myLastName === "smith") {
-    // do this if the if expression is true
-    console.log("I know you");
-} else {
-    // do this if the if expression IS FALSE
-    console.log("I don't know you");
+
+let myValue = 1;
+
+let done = false;
+
+// start the command loop
+while(!done) {
+    // show a menu of options (FINISHED)
+    console.log("Hi there");
+    console.log("enter 'quit' to exit");
+    console.log("enter '1' to print myValue");
+    console.log("enter '2' to increment myValue");
+
+    // wait for user to pick
+    // get input from the user
+    const userChoice = prompt("Enter you choice");
+
+    // process the userChoice
+    if(userChoice === "quit" || userChoice === "stop") {
+        done = true;
+    } else if(userChoice === "1") {
+        console.log(`myValue = ${myValue}`);
+    } else if(userChoice === "2") {
+        myValue++;
+    } else {
+        console.log("Did not understand your choice: " + userChoice);
+    }
+
+    // start loop over if we didnt exit
 }
 
-// loop through last name printing out each character
-// when test is false, loop is over
-//      init       test                update
-for (let i = 0; i <= myLastName.length; i++) {
-    // print out the ith character in myLastName
-    console.log(myLastName.charAt(i));
-}
-// after loop test is false, execution resumes here
-
-// print the lastname backwards
-//       s  m  i  t  h
-// index 0  1  2  3  4
-// start loop with index 4 and go down to 0
-for (let i = myLastName.length - 1; i >= 0; i--) {
-    console.log(myLastName.charAt(i));
-}
+// after quitting keep going from here
+console.log("program is over")
