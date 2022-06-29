a29e2d958ad93631adb76bca0bd82caaf4803112
Docrob <mark.robinson@codeup.com>
2022-06-29 08:56:48 -0500

    jun 29 morning work

--- practice/jun28.js
+++ practice/jun28.js
@@ -1,43 +1,55 @@
-/*
-COMMAND LOOPS
+"use strict";
 
-a loop that:
- 1. accepts a command from the user
- 2. acts on the command if the command is valid
- 3. continues looping, waiting for the next command
- 4. EXITS when the user issues an exit command
+(function() {
 
- */
+    let myValue = 1;
+    let done = false;
 
-let myValue = 1;
 
-let done = false;
+    /*
+    COMMAND LOOPS
 
-// start the command loop
-while(!done) {
-    // show a menu of options (FINISHED)
-    console.log("Hi there");
-    console.log("enter 'quit' to exit");
-    console.log("enter '1' to print myValue");
-    console.log("enter '2' to increment myValue");
+    a loop that:
+     1. accepts a command from the user
+     2. acts on the command if the command is valid
+     3. continues looping, waiting for the next command
+     4. EXITS when the user issues an exit command
 
-    // wait for user to pick
-    // get input from the user
-    const userChoice = prompt("Enter you choice");
+     */
 
-    // process the userChoice
-    if(userChoice === "quit" || userChoice === "stop") {
-        done = true;
-    } else if(userChoice === "1") {
-        console.log(`myValue = ${myValue}`);
-    } else if(userChoice === "2") {
-        myValue++;
-    } else {
-        console.log("Did not understand your choice: " + userChoice);
+    // start the command loop. loop until done is TRUE
+    while(!done) {
+        printMenu();
+
+        // wait for user to pick
+        // get input from the user
+        const userChoice = prompt("Enter you choice");
+
+        // process the userChoice
+        processUserChoice(userChoice);
     }
 
-    // start loop over if we didnt exit
-}
+    // after quitting keep going from here
+    console.log("program is over");
 
-// after quitting keep going from here
-console.log("program is over")
+    function printMenu() {
+        // show a menu of options (FINISHED)
+        console.log("Hi there");
+        console.log("enter 'quit' to exit");
+        console.log("enter '1' to print myValue");
+        console.log("enter '2' to increment myValue");
+    }
+
+    function processUserChoice(userChoice) {
+        if (userChoice === "quit" || userChoice === "stop") {
+            done = true;
+        } else if (userChoice === "1") {
+            console.log(`myValue = ${myValue}`);
+        } else if (userChoice === "2") {
+            myValue++;
+        } else {
+            console.log("Did not understand your choice: " + userChoice);
+        }
+        return done;
+    }
+})();
---
+++ practice/jun29.js
@@ -0,0 +1,57 @@
+"use strict";
+
+(function() {
+
+    // GUESSING GAME
+    // computer starts with # from 1 to 10
+    let answer = randomNumberInRange(1, 10);
+    console.log("answer is " + answer);
+
+    let done = false;
+
+
+    /*
+    COMMAND LOOPS
+
+    a loop that:
+     1. user guesses a number from 1 to 10
+     2. if user choice < answer, print TOO LOW
+     3. if user choice > answer, print TOO HIGH
+     4. EXITS when the user guesses the # OR issues an exit command
+
+     */
+
+    // start the command loop. loop until done is TRUE
+    while(!done) {
+        // wait for user to pick
+        // get input from the user
+        const userChoice = prompt("Guess a number from 1 to 10. Enter 'quit' to exit");
+
+        // process the userChoice
+        processUserChoice(userChoice);
+    }
+
+    // after quitting keep going from here
+    console.log("program is over");
+
+    function processUserChoice(userChoice) {
+        if (userChoice === "quit" || userChoice === "stop") {
+            done = true;
+        } else if (userChoice < answer) {
+            console.log(`TOO LOW`);
+        } else if (userChoice > answer) {
+            console.log("TOO HIGH")
+        } else if (userChoice == answer) {
+            console.log("CORRECT!");
+            done = true;
+        } else {
+            console.log("Did not understand your choice: " + userChoice);
+        }
+    }
+
+    function randomNumberInRange(minVal, maxVal) {
+        // inclusive
+        return Math.floor(Math.random() * maxVal) + minVal;
+    }
+
+})();
