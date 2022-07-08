4b00247490e556f6a789a5c9076e57a31f236d14
Docrob <mark.robinson@codeup.com>
2022-07-08 09:36:48 -0500

    attributes exercise done

---
+++ practice/jul7.js
@@ -0,0 +1,11 @@
+
+let searchFor = ["red", "blue"];
+let searchIn = ["cat", "red", "blue", "green"];
+
+console.log(searchFor.toString());
+console.log(searchIn.toString());
+
+console.log(searchIn.toString().includes(searchFor.toString()));
+
+document.querySelector("h1").style.border = "1px red solid";
+
